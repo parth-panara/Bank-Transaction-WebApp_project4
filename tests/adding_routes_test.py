f45d0a3c3e49314069b48401b02326176a8200a2
Parth Panara <parthpanara9@gmail.com>
2022-05-02 03:31:06 -0400

    manage account page generated and tested#12

--- tests/adding_routes_test.py
+++ tests/adding_routes_test.py
@@ -6,10 +6,20 @@ def test_request_main_menu_links(client):
     assert response.status_code == 200
 
 # user routes checked on profile edit test#11
+
 def test_user_user_profile(client, application):
     response = client.post('/profile', data={
         'email': 'parth@webizly.com',
         'password': 'testtest',
         'confirm': 'testtest'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+
+# user routes checked on manage account where user can change password after login test#12
+
+def test_user_manage_account(client, application):
+    response = client.post('/account', data={
+        'password': 'testtest12',
+        'confirm': 'testtest12'
     }, follow_redirects=True)
     assert response.status_code == 200

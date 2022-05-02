bcdae189ce1087e0670a7cd7b67fbc238b6154b8
Parth Panara <parthpanara9@gmail.com>
2022-05-01 23:03:33 -0400

    generated about page and tested #2

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,7 +4,7 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-
+    assert b'href="/about"' in response.data
 
 
 
@@ -15,3 +15,9 @@ def test_request_index(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b"Home" in response.data
+
+def test_request_about(client):
+    """This makes the about page"""
+    response = client.get("/about")
+    assert response.status_code == 200
+    assert b"About" in response.data

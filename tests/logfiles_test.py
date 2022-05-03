a89257cccf85e32762e2c678e1fc86a6fa1614fc
Parth Panara <parthpanara9@gmail.com>
2022-05-03 03:42:42 -0400

    previous commit's missed log init files are pushed. logfile and its readability tested#19

---
+++ tests/logfiles_test.py
@@ -0,0 +1,34 @@
+"""This test the homepage"""
+
+"""This test the all logfiles"""
+import logging
+import os
+
+
+import app.config
+
+
+#test to check if handler.log file exists#20
+def test_logfile_handler():
+    root = os.path.dirname(os.path.abspath(__file__))
+    logfile= os.path.join(root, '../logs/handler.log')
+    assert os.path.exists(logfile) == True
+
+#second way to check if handler.log file exists#20
+def test_handler_logfile():
+    log_dir = app.config.Config.LOG_DIR
+    filepath = os.path.join(log_dir, "handler.log")
+    assert os.path.isfile(filepath)
+
+
+# test to check if the functions of logfiles are from 'init_py' is readable#20
+
+def test_add_path_to_logfile():
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.join(path, '../app/logging_config/__init__.py')
+    with open(filepath, encoding="utf-8") as file:
+        data = {
+            'file': filepath
+            }
+    assert os.path.isfile(filepath)
